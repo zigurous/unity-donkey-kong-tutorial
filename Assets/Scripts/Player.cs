faab9da5aabc284a1ba723a6ce4dd2f5f87b544e
Adam Graham <adam@zigurous.com>
2024-07-13 15:01:36 -0500

    Implement player movement

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,87 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private new Rigidbody2D rigidbody;
+    private new Collider2D collider;
+
+    private Collider2D[] overlaps = new Collider2D[4];
+    private Vector2 direction;
+
+    private bool grounded;
+    private bool climbing;
+
+    public float moveSpeed = 2f;
+    public float jumpStrength = 4f;
+
+    private void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+        collider = GetComponent<Collider2D>();
+    }
+
+    private void Update()
+    {
+        CheckCollision();
+        SetDirection();
+    }
+
+    private void CheckCollision()
+    {
+        grounded = false;
+        climbing = false;
+
+        Vector3 size = collider.bounds.size;
+        size.y += 0.1f;
+        size.x /= 2f;
+
+        int amount = Physics2D.OverlapBoxNonAlloc(transform.position, size, 0, overlaps);
+
+        for (int i = 0; i < amount; i++)
+        {
+            GameObject hit = overlaps[i].gameObject;
+
+            if (hit.layer == LayerMask.NameToLayer("Ground"))
+            {
+                // Only set as grounded if the platform is below the player
+                grounded = hit.transform.position.y < (transform.position.y - 0.5f);
+
+                // Turn off collision on platforms the player is not grounded to
+                Physics2D.IgnoreCollision(overlaps[i], collider, !grounded);
+            }
+            else if (hit.layer == LayerMask.NameToLayer("Ladder"))
+            {
+                climbing = true;
+            }
+        }
+    }
+
+    private void SetDirection()
+    {
+        if (climbing) {
+            direction.y = Input.GetAxis("Vertical") * moveSpeed;
+        } else if (grounded && Input.GetButtonDown("Jump")) {
+            direction = Vector2.up * jumpStrength;
+        }
+
+        direction += Physics2D.gravity * Time.deltaTime;
+        direction.x = Input.GetAxis("Horizontal") * moveSpeed;
+
+        // Prevent gravity from building up infinitely
+        if (grounded) {
+            direction.y = Mathf.Max(direction.y, -1f);
+        }
+
+        if (direction.x >= 0f) {
+            transform.eulerAngles = Vector3.zero;
+        } else {
+            transform.eulerAngles = new Vector3(0f, 180f, 0f);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        rigidbody.MovePosition(rigidbody.position + direction * Time.fixedDeltaTime);
+    }
+
+}
